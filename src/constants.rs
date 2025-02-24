78e7e9d66cb7e7027834fd84d61975c8c4791b33
Kornel <kornel@geekhood.net>
2025-02-24 21:50:21 +0000

    I have _opinions_ about these now

--- src/constants.rs
+++ src/constants.rs
@@ -1,33 +1,45 @@
+/// `Bt709` works for sRGB images.
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 #[non_exhaustive]
 pub enum ColorPrimaries {
-    /// ITU-R BT1361
+    /// Rec.709 and sRGB
     Bt709 = 1,
     Unspecified = 2,
     /// ITU-R BT601-6 525
     Bt601 = 6,
     /// ITU-R BT2020
     Bt2020 = 9,
-    /// SMPTE ST 431-2
+    /// SMPTE ST 431-2. NB: "P3" images use DisplayP3 instead.
     DciP3 = 11,
     /// SMPTE ST 432-1
     DisplayP3 = 12,
 }
 
+/// This controls how color data is interpreted (gamma).
+///
+/// If you don't know what to do with these, pick `Srgb`.
+///
+/// Reasonable options include `Bt709` (HDTV), `Bt2020_10` (Wide Gamut), `Smpte2084`, `Hlg` (HDR).
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 #[non_exhaustive]
 pub enum TransferCharacteristics {
-    /// ITU-R BT1361
+    /// Rec.709. May be appropriate for conversions from video.
     Bt709 = 1,
+    /// Don't use this for color channels.
     Unspecified = 2,
-    /// BT.470 System M (historical)
+    /// Don't use this. Analog NTSC TV. BT.470 System M (historical)
+    #[deprecated(note = "This is obsolete. Please don't proliferate legacy baggage.")]
+    #[doc(hidden)]
     Bt470M = 4,
-    /// BT.470 System B, G (historical)
-    Bt470BG,
-    /// ITU-R BT601-6 525
+    /// Don't use this. Analog PAL TV. BT.470 System B, G (historical)
+    #[deprecated(note = "This is obsolete. Please don't proliferate legacy baggage.")]
+    #[doc(hidden)]
+    Bt470BG = 5,
+    /// ITU-R BT601-6 525. Not recommended, unless you're converting from unlabelled low-res video clips.
+    /// See `Bt709` and `Srgb`.
     Bt601 = 6,
-    /// SMPTE 240 M
-    Smpte240,
+    /// Don't use this. SMPTE 240 M. It's just a worse Rec.709.
+    Smpte240 = 7,
     /// "Linear transfer characteristics"
     Linear = 8,
     /// "Logarithmic transfer characteristic (100:1 range)"
@@ -35,32 +47,35 @@ pub enum TransferCharacteristics {
     /// "Logarithmic transfer characteristic (100 * Sqrt(10) : 1 range)"
     LogSqrt = 10,
     /// IEC 61966-2-4
-    Iec61966,
-    /// BT.1361 extended color gamut system (historical)
-    Bt1361,
-    /// sRGB
+    Iec61966 = 11,
+    /// Don't use this. Obsoleted BT.1361 extended color gamut system (historical)
+    #[deprecated(note = "This is obsolete. Please don't proliferate legacy baggage.")]
+    #[doc(hidden)]
+    Bt1361 = 12,
+    /// sRGB. This is the safe choice for encoding "standard" RGB images, especially 8-bit inputs.
     Srgb = 13,
-    /// ITU-R BT2020 for 10-bit system
+    /// ITU-R BT2020 for 10-bit system. Reasonable for encoding wide gamut.
     Bt2020_10 = 14,
     /// ITU-R BT2020 for 12-bit system
     Bt2020_12 = 15,
     /// SMPTE ST 2084, ITU BT.2100 PQ
-    Smpte2084,
-    /// SMPTE ST 428
-    Smpte428,
+    Smpte2084 = 16,
+    /// SMPTE ST 428. Not recommended. Overkill for images. Use `Bt2020_10` instead.
+    Smpte428 = 17,
     /// BT.2100 HLG (Hybrid Log Gamma), ARIB STD-B67
-    Hlg,
+    Hlg = 18,
 }
 
+/// This is the format of color channels.
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 #[non_exhaustive]
 pub enum MatrixCoefficients {
-    /// GBR (sRGB)
+    /// GBR (sRGB). This isn't actually good for most RGB images. Use `Bt709` for lossy and `Ycgco` for lossless.
     Rgb = 0,
     /// ITU-R BT1361
     Bt709 = 1,
     Unspecified = 2,
-    /// ITU-R BT601-6 525
+    /// ITU-R BT601-6 525. This matches luma in JPEG's YCbCr when used with sRGB transfer characteristics, but is a bit off for chroma.
     Bt601 = 6,
     Ycgco = 8,
     /// ITU-R BT2020 non-constant luminance system
